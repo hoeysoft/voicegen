706341e4a4013d0dd879e4ba71778e9250b7d770
hoey <nznial@gmail.com>
2015-08-11 22:03:35 +0900

    Initial Commit

---
+++ voicegen/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  voicegen
+//
+//  Created by KIMYEONGHO on 8/11/15.
+//  Copyright (c) 2015 KIMYEONGHO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
